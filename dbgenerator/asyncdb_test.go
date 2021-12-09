78db1fd868b7e0f4de131ddc49ec44cbb6432ace
xu <937134366@qq.com>
2021-12-09 15:20:25 +0800

    asyncdb complete

--- dbgenerator/asyncdb_test.go
+++ dbgenerator/asyncdb_test.go
@@ -1,27 +1,29 @@
 package main
 
 import (
-	"go/ast"
+	"robot/js"
 	"testing"
 )
 
 func Test_parsePackage(t *testing.T) {
-	pkg := parsePackage([]string{"D:/work/P/robot/db/"}, nil)
-	for i := range pkg.GoFiles {
-		syntax := pkg.Syntax[i]
-		for _, decl := range syntax.Decls {
-			if funcDecl, ok := decl.(*ast.FuncDecl); ok {
-
-				for _, field := range funcDecl.Type.Params.List {
-					for _, name := range field.Names {
-						objDecl := name.Obj.Decl.(*ast.Field)
-						cut := sourceCut(pkg.Fset, objDecl.Type)
-						t.Log(cut)
-					}
-				}
-			}
-		}
-	}
+	pkg := parsePackage([]string{"D:/work/P/Server/LeafServer/src/server/db/player_base.go"}, nil)
+	decls := parseFuncDecls(pkg.Syntax[0])
+	t.Log(js.IdentJson(decls))
+	//for i := range pkg.GoFiles {
+	//	syntax := pkg.Syntax[i]
+	//	for _, decl := range syntax.Decls {
+	//		if funcDecl, ok := decl.(*ast.FuncDecl); ok {
+	//
+	//			for _, field := range funcDecl.Type.Params.List {
+	//				for _, name := range field.Names {
+	//					objDecl := name.Obj.Decl.(*ast.Field)
+	//					cut := sourceCut(pkg.Fset, objDecl.Type)
+	//					t.Log(cut)
+	//				}
+	//			}
+	//		}
+	//	}
+	//}
 }
 
 func TestGenAsync(t *testing.T) {
