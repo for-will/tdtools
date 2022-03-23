76bc730052a8cf66f588596bc9d78988115f9097
xu <937134366@qq.com>
2022-03-23 18:49:14 +0800

    重构gensql

---
+++ gensql/game_sql.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/tools/go/packages"
+	"log"
+)
+
+type GameSql struct {
+	Models   []*Model
+	OnModel  *Model
+	PkgDir   string
+	SrcFiles []string
+}
+
+const (
+	GameDbDir              = "D:/work/P/Server/LeafServer/src/server/db/"
+	LootMissionGoFileName  = "lootmission.go"
+	CrystalGoFileName      = "crystal.go"
+	SeasonTaskGoFileName   = "season_task.go"
+	SeasonPlayerGoFileName = "season_player.go"
+	SeasonRewardGoFileName = "season_reward.go"
+)
+
+func (g *GameSql) ReloadPackage() *packages.Package {
+	return loadPackage(g.PkgDir, g.SrcFiles...)
+}
+
+func (g *GameSql) Init(ModelName string, PkgDir string, Files ...string) {
+	g.PkgDir = PkgDir
+	g.SrcFiles = Files
+
+	pkg := g.ReloadPackage()
+	for _, syntax := range pkg.Syntax {
+		g.Models = append(g.Models, parseModel(syntax)...)
+	}
+	g.OnModel = g.GetModel(ModelName)
+}
+
+func (g *GameSql) GetModel(name string) *Model {
+	for _, m := range g.Models {
+		if m.Name == name {
+			return m
+		}
+	}
+	log.Fatalf("GetModel %s nil", name)
+	return nil
+}
+
+func (g *GameSql) GenerateMethod(MethodName string, Generator func(model *Model, MethodName string) string) {
+	MethodSrc := Generator(g.OnModel, MethodName)
+	editFunction(g.ReloadPackage(), MethodName, MethodSrc)
+}
+
+func (g *GameSql) BuildSeasonTask() {
+
+	g.Init("SeasonTask", GameDbDir, SeasonTaskGoFileName)
+
+	g.GenerateMethod("LoadSeasonTasks", func(model *Model, MethodName string) string {
+		return model.DbSelect().Where(model.FieldEqualCond("PlayerSn")).
+			GenFixedQueryFunc(MethodName)
+	})
+
+	g.GenerateMethod("BatchInsertSeasonTask", func(model *Model, MethodName string) string {
+		return model.GenBatchInsertFunc()
+	})
+
+	g.GenerateMethod("UpdateSeasonTaskProgress", func(model *Model, MethodName string) string {
+		return model.GenUpdateFunc(MethodName, "Progress", "Status", "Looped")
+	})
+
+	g.GenerateMethod("ResetSeasonTask", func(model *Model, MethodName string) string {
+		return model.Where(&FieldEqual{Field: model.GetField("PlayerSn")}).
+			GenUpdateFunc(MethodName, "Progress", "Status", "Looped")
+	})
+}
+
+func (g *GameSql) BuildLootMission() {
+	g.Init("LootMission", GameDbDir, LootMissionGoFileName)
+
+	g.GenerateMethod("LoadLootMissions", func(model *Model, MethodName string) string {
+		return model.DbSelect().Where(model.FieldEqualCond("PlayerSn")).
+			GenFixedQueryFunc(MethodName)
+	})
+}
+
+func (g *GameSql) BuildCrystal() {
+	g.Init("Crystal", GameDbDir, CrystalGoFileName)
+
+	g.GenerateMethod("GetPlayerCrystals", func(model *Model, MethodName string) string {
+		return model.DbSelect().Where(model.FieldEqualCond("PlayerId")).
+			GenFixedQueryFunc(MethodName)
+	})
+
+	g.GenerateMethod("BatchInsertCrystal", func(model *Model, MethodName string) string {
+		return model.GenBatchInsertFunc()
+	})
+
+	g.GenerateMethod("CreateCrystal", func(model *Model, MethodName string) string {
+		return model.GenCreateFunc()
+	})
+
+	g.GenerateMethod("UpdateCrystal", func(model *Model, MethodName string) string {
+		return model.GenUpdateFunc(MethodName, "Locked", "Lv", "Expr")
+	})
+
+	g.GenerateMethod("DeleteCrystals", func(model *Model, MethodName string) string {
+		return model.Where(model.FieldInCond("Id")).GenDeleteFunc(MethodName)
+	})
+}
+
+func (g *GameSql) BuildSeasonPlayer() {
+	g.Init("SeasonPlayer", GameDbDir, SeasonPlayerGoFileName)
+
+	g.GenerateMethod("NewTblSeasonPlayer", func(model *Model, MethodName string) string {
+		return model.GenNewTblFunc()
+	})
+
+	g.GenerateMethod("LoadSeasonPlayer", func(model *Model, MethodName string) string {
+		return model.DbSelect().Where(model.FieldEqualCond("PlayerSn")).
+			GenFixedQueryFunc(MethodName)
+	})
+
+	g.GenerateMethod("CreateSeasonPlayer", func(model *Model, MethodName string) string {
+		return model.GenCreateFunc()
+	})
+
+	g.GenerateMethod("UpdateSeasonPlayerFields", func(model *Model, MethodName string) string {
+		return model.GenUpdateFunc(MethodName,
+			"SeasonId", "Premium", "SeasonExp", "TodayExp", "DayTimeOut", "WeekTimeOut", "SeasonTimeOut")
+	})
+}
+
+func (g GameSql) BuildSeasonReward() {
+	g.Init("SeasonReward", GameDbDir, SeasonRewardGoFileName)
+
+	g.GenerateMethod("LoadSeasonReward", func(model *Model, MethodName string) string {
+		return model.DbSelect().Where(model.FieldEqualCond("PlayerSn")).
+			GenFixedQueryFunc(MethodName)
+	})
+
+	g.GenerateMethod("BatchInsertSeasonReward", func(model *Model, MethodName string) string {
+		return model.GenBatchInsertFunc()
+	})
+
+	g.GenerateMethod("UpdateSeasonReward", func(model *Model, MethodName string) string {
+		return model.GenUpdateFunc(MethodName, "Base", "Premium")
+	})
+}
--- gensql/main.go
+++ gensql/main.go
@@ -2,120 +2,14 @@ package main
 
 import (
 	_ "github.com/go-sql-driver/mysql"
-	"golang.org/x/tools/go/packages"
-	"log"
 )
 
 func main() {
 
-	var reloadPackage = func() *packages.Package {
-		return loadPackage("D:/work/P/Server/LeafServer/src/server/db/",
-			"lootmission.go", "crystal.go", "season_task.go", "season_player.go", "season_reward.go")
-	}
-	pkg := reloadPackage()
-
-	var models []*Model
-	for _, syntax := range pkg.Syntax {
-		models = append(models, parseModel(syntax)...)
-	}
-
-	var getModel = func(name string) *Model {
-		for _, m := range models {
-			if m.Name == name {
-				return m
-			}
-		}
-		log.Fatalf("getModel %s nil", name)
-		return nil
-	}
-
-	var genFunc = func(name string, gen func(model *Model)) {
-		if m := getModel(name); m != nil {
-			gen(m)
-		}
-	}
-
-	//LootMission
-	//genFunc("LootMission", func(model *Model) {
-	//	LoadLootMissions := model.DbSelect().
-	//		Where(model.FieldEqualCond("PlayerSn")).
-	//		GenFixedQueryFunc("LoadLootMissions")
-	//	editFunction(pkg, "LoadLootMissions", LoadLootMissions)
-	//})
-
-	// Crystal
-	//genFunc("Crystal", func(model *Model) {
-	//	GetPlayerCrystals := model.DbSelect().
-	//		Where(model.FieldEqualCond("PlayerId")).
-	//		GenFixedQueryFunc("GetPlayerCrystals")
-	//	editFunction(reloadPackage(), "GetPlayerCrystals", GetPlayerCrystals)
-	//
-	//	BatchInsertCrystal := model.GenBatchInsertFunc()
-	//	editFunction(reloadPackage(), "BatchInsertCrystal", BatchInsertCrystal)
-	//
-	//	CreateCrystal := model.GenCreateFunc()
-	//	editFunction(reloadPackage(), "CreateCrystal", CreateCrystal)
-	//
-	//	UpdateCrystal := model.GenUpdateFunc("UpdateCrystal", "Locked", "Lv", "Expr")
-	//	editFunction(reloadPackage(), "UpdateCrystal", UpdateCrystal)
-	//
-	//	DeleteCrystals := model.Where(model.FieldInCond("Id")).GenDeleteFunc("DeleteCrystals")
-	//	editFunction(reloadPackage(), "DeleteCrystals", DeleteCrystals)
-	//})
-
-	//SeasonTask
-	genFunc("SeasonTask", func(model *Model) {
-		LoadSeasonTasks := model.DbSelect().
-			Where(model.FieldEqualCond("PlayerSn")).
-			GenFixedQueryFunc("LoadSeasonTasks")
-		editFunction(pkg, "LoadSeasonTasks", LoadSeasonTasks)
-
-		BatchInsertSeasonTask := model.GenBatchInsertFunc()
-		editFunction(reloadPackage(), "BatchInsertSeasonTask", BatchInsertSeasonTask)
-
-		UpdateSeasonTaskProgress := model.GenUpdateFunc("UpdateSeasonTaskProgress", "Progress", "Status", "Looped")
-		editFunction(reloadPackage(), "UpdateSeasonTaskProgress", UpdateSeasonTaskProgress)
-
-		var Func string
-		Func = model.Where(&FieldEqual{Field: model.GetField("PlayerSn")}).
-			GenUpdateFunc("ResetSeasonTask", "Progress", "Status", "Looped")
-		editFunction(reloadPackage(), "ResetSeasonTask", Func)
-	})
-
-	// SeasonPlayer
-	genFunc("SeasonPlayer", func(model *Model) {
-
-		var Func string
-
-		Func = model.GenNewTblFunc()
-		editFunction(reloadPackage(), "NewTblSeasonPlayer", Func)
-
-		LoadSeasonPlayer := model.DbSelect().
-			Where(model.FieldEqualCond("PlayerSn")).
-			GenFixedQueryFunc("LoadSeasonPlayer")
-		editFunction(reloadPackage(), "LoadSeasonPlayer", LoadSeasonPlayer)
-
-		CreateSeasonPlayer := model.GenCreateFunc()
-		editFunction(reloadPackage(), "CreateSeasonPlayer", CreateSeasonPlayer)
-
-		UpdateSeasonPlayerFields := model.GenUpdateFunc("UpdateSeasonPlayerFields",
-			"SeasonId", "Premium", "SeasonExp", "TodayExp", "DayTimeOut", "WeekTimeOut", "SeasonTimeOut")
-		editFunction(reloadPackage(), "UpdateSeasonPlayerFields", UpdateSeasonPlayerFields)
-	})
-
-	// SeasonReward
-	genFunc("SeasonReward", func(model *Model) {
-		var Func string
-		Func = model.DbSelect().
-			Where(model.FieldEqualCond("PlayerSn")).
-			GenFixedQueryFunc("LoadSeasonReward")
-		editFunction(pkg, "LoadSeasonReward", Func)
-
-		Func = model.GenBatchInsertFunc()
-		editFunction(reloadPackage(), "BatchInsertSeasonReward", Func)
-
-		Func = model.GenUpdateFunc("UpdateSeasonReward",
-			"Base", "Premium")
-		editFunction(reloadPackage(), "UpdateSeasonReward", Func)
-	})
+	game := &GameSql{}
+	//game.BuildLootMission()
+	//game.BuildCrystal()
+	game.BuildSeasonTask()
+	game.BuildSeasonPlayer()
+	game.BuildSeasonReward()
 }
